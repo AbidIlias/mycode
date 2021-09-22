4e64731bab8356397eef04d834a1275a68634922
AbidIlias <ai25@ntrs.com>
2021-09-22 18:27:33 +0000

    Lab 27 Parsing Log Files

--- github/push_it.py
+++ github/push_it.py
@@ -9,17 +9,34 @@ imperfect. The intent is for you to gaze upon the building blocks and engage in
 critcal thinking for the next iteration...
 '''
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
+import getopt
 import os
-version           = '1.2'
-commit_message    = input('Commit Comment: ')
-#working_directory = 'cd ~/mycode'
-working_directory = '/home/student/mycode'
-git_add           = 'git add *'
-git_commit        = 'git commit -m "' + commit_message + '"' 
-git_push          = 'git push origin'
-#os.system(working_directory)
-os.chdir(working_directory)
-os.system(git_add)
-os.system(git_commit)
-os.system(git_push)
+import sys
+def main():
+  argumentList = sys.argv[1:]
+  options      = "v"
+  long_options = ["version"]
+  version      = '1.4'
+  try:
+    arguments, values = getopt.getopt(argumentList, options, long_options)
+    for currentArgument, currentValue in arguments:
+      if currentArgument in ("-v", "--version"):
+        print ("pushit " + version)
+        break
+  except:
+    pass
+  if len(sys.argv) == 1:
+    commit_message    = input('Commit Comment: ')
+    #working_directory = 'cd ~/mycode'
+    working_directory = '/home/student/mycode'
+    git_add           = 'git add *'
+    git_commit        = 'git commit -m "' + commit_message + '"'
+    git_push          = 'git push origin'
+    #os.system(working_directory)
+    os.chdir(working_directory)
+    os.system(git_add)
+    os.system(git_commit)
+    os.system(git_push)
+if __name__ == "__main__":
+    main()
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
